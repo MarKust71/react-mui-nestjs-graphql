8440626fc7ee75c493f5ea610a04a58bc4c3b9ac
Marek Kustosz <markust71@hotmail.com>
2022-01-06 21:23:06 +0100

    Merge pull request #3
    * add helmet
    
    * connect firebase

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import 'dotenv/config';
+import { firebaseConfig } from './config/firebase.config';
 
 async function bootstrap() {
+  if (!admin.apps.length) {
+    admin.initializeApp(firebaseConfig);
+  }
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
