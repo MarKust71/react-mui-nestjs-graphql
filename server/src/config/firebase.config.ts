dd6d75c87c14a7f477319ad54bf76cdb1172cce7
Marek Kustosz <markust71@hotmail.com>
2022-01-06 21:20:56 +0100

    connect firebase

---
+++ server/src/config/firebase.config.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+
+const {
+  FIREBASE_APIKEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} = process.env;
+
+export const firebaseConfig = {
+  apiKey: FIREBASE_APIKEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
+};
